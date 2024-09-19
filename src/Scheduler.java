170c5f448f3d0105a9e199a0c863954b53fa3ea3
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-09-19 15:22:39 -0400

    added scanner and quit functionality

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Scheduler {
+    public Scheduler(){
+
+    }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        boolean exited = false;
+        while(!exited) // to satisfy the entry condition
+        {
+            switch(scanner.nextLine()){
+                case "Q":
+                    exited = true;
+                    break;
+                case "PP":
+                    System.out.println("No appointments yet");
+                    break;
+                default:
+                    System.out.println("Invalid appointment");
+            }
+        }
+        System.out.println("Scheduler is terminated.");
+    }
 }
