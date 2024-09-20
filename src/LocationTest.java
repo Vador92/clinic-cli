14f046ec15089463d8717eeafb522d31191716eb
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-09-19 23:49:46 -0400

    added more test cases to Location

--- src/LocationTest.java
+++ src/LocationTest.java
@@ -10,4 +10,18 @@ class LocationTest {
 
         assertEquals(testLocation.toString(), "Union County,07066");
     }
+
+    @Test
+    void exampleLocationTest2(){
+        Location testLocation = Location.BRIDGEWATER;
+        assertEquals(testLocation, Location.BRIDGEWATER);
+        assertEquals(testLocation.toString(), "Somerset County,08807");
+    }
+
+    @Test
+    void exampleLocationTest3(){
+        Location testLocation = Location.EDISON;
+        assertEquals(testLocation, Location.EDISON);
+        assertEquals(testLocation.toString(), "Middlesex County,08817");
+    }
 }
