f3d3a75bb0e560fec182f6a050257d22785ec3d3
Yuet Yue <yuetyue0521@gmail.com>
2024-09-20 12:28:03 -0400

    added compareTo logic to eliminate duplicates and makes sure they are not duplicate objects

--- src/Profile.java
+++ src/Profile.java
@@ -29,7 +29,18 @@ public class Profile implements Comparable<Profile> {
 
 
     @Override
-    public int compareTo(Profile o) {
-        return 0;
+    public int compareTo(Profile other) {
+        int lnameCompare = lname.compareToIgnoreCase(other.lname); //checks for duplicate lnames
+        if(lnameCompare != 0) {
+            return lnameCompare; //if duplicates not found return lname
+        }
+
+        int fnameCompare = fname.compareToIgnoreCase(other.fname);
+        if(fnameCompare != 0) {
+            return fnameCompare;
+        }
+
+        return dob.compareTo(other.getDob);
+
     }
 }
