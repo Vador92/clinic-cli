f530ffb92b71a21c60e64732a3898459b2c76979
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-09-24 15:27:45 -0400

    fixed specialty class

--- src/Specialty.java
+++ src/Specialty.java
@@ -1,18 +1,17 @@
 public enum Specialty {
-    FAMILY("Family", 250),
-    PEDIATRICIAN("Pediatrician", 300),
-    ALLERGIST("Allergist", 350);
+    FAMILY(250),
+    PEDIATRICIAN(300),
+    ALLERGIST( 350);
 
-    private final String Specialty;
     private final int charge;
 
-    Specialty(String Specialty, int charge) {
-        this.Specialty = Specialty;
+    Specialty(int charge) {
         this.charge = charge;
     }
 
-    public String getSpecialty() {
-        return Specialty;
+    @Override
+    public String toString(){
+        return name();
     }
 
     public int getCharge() {
