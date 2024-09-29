8a26bdfed3f3566ea6c2a87cbdba5eb3d7139d19
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-09-29 19:54:24 -0400

    finished javadoc for specialty

--- src/Specialty.java
+++ src/Specialty.java
@@ -1,5 +1,6 @@
 /**
- * @author Varun
+ * This is an enum class for the Speciality of Provider with the charges.
+ * @author Varun Doreswamy
  */
 public enum Specialty {
     FAMILY(250),
@@ -8,15 +9,27 @@ public enum Specialty {
 
     private final int charge;
 
+    /**
+     * Constructor for Specialty
+     * @param charge used to set the charge of the provider
+     */
     Specialty(int charge) {
         this.charge = charge;
     }
 
+    /**
+     * Returns the name of the enum using name()
+     * @return String of enums name
+     */
     @Override
     public String toString(){
         return name();
     }
 
+    /**
+     * Gets the charge of the enum
+     * @return int of the charge for specific specialty
+     */
     public int getCharge() {
         return charge;
     }
