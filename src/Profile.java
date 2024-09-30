7009cbdf9b807a9cbeab02da4477c4754b2deecd
Yuet Yue <yuetyue0521@gmail.com>
2024-09-29 23:45:00 -0400

    drafted up new code for the compareTo() method, going to eliminate magic numbers in my next push

--- src/Profile.java
+++ src/Profile.java
@@ -71,7 +71,7 @@ public class Profile implements Comparable<Profile> {
     // need to change compareTo method
     @Override
     public int compareTo(Profile other) {
-        int lnameCompare = lname.compareToIgnoreCase(other.lname); //checks for duplicate lnames
+        /*int lnameCompare = lname.compareToIgnoreCase(other.lname); //checks for duplicate lnames
         if (lnameCompare != 0) {
             return lnameCompare; //if duplicates not found return lname
         }
@@ -81,7 +81,34 @@ public class Profile implements Comparable<Profile> {
             return fnameCompare;
         }
 
-        return dob.compareTo(other.dob);
+        return dob.compareTo(other.dob);*/
+
+        // Compare last names first
+        int lnameCompare = lname.compareToIgnoreCase(other.lname);
+        if (lnameCompare > 0) {
+            return 1;  // Current last name is greater
+        } else if (lnameCompare < 0) {
+            return -1; // Current last name is smaller
+        }
+
+        // If last names are equal, compare first names
+        int fnameCompare = fname.compareToIgnoreCase(other.fname);
+        if (fnameCompare > 0) {
+            return 1;  // Current first name is greater
+        } else if (fnameCompare < 0) {
+            return -1; // Current first name is smaller
+        }
+
+        // If both last and first names are equal, compare date of birth
+        int dobCompare = dob.compareTo(other.dob);
+        if (dobCompare > 0) {
+            return 1;  // Current date of birth is later (greater)
+        } else if (dobCompare < 0) {
+            return -1; // Current date of birth is earlier (smaller)
+        }
+
+        // If all fields (last name, first name, and date of birth) are equal
+        return 0;
     }
 
 
