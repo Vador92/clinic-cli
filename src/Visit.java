2b5306ce99e71c8ef01ab48ec15c950dd681836f
Yuet Yue <yuetyue0521@gmail.com>
2024-09-30 20:27:55 -0400

    finalized JavaDoc for the Visit Class

--- src/Visit.java
+++ src/Visit.java
@@ -1,32 +1,50 @@
 /**
- *
- * @author Yuet
+ * This is the Visit class which tracks a patient's visits
+ * The individual patient's visits are stored within a linked list
+ * @author Yuet Yue
  */
-// Linked list for the visits
+
 public class Visit {
+
+    // instance variables
     private Appointment appointment;
     private Visit next;
 
-    // creates a singular node that will eventually form a singly linked list when more nodes are added
+    /**
+     * This method creates a node object that represents a patient's visit
+     * @param appointment used to contain the date information of appointment
+     * @param next returns the node address that contains the next visit
+     */
     public Visit(Appointment appointment, Visit next) {
         this.appointment = appointment;
         this.next = next;
     }
 
-    // getter method for the next node in the linked list
+    /**
+     * This method acts as a getter for the next node in visit tracking
+     * The next node contains another appointment's date
+     * @return the address of the next node
+     */
     public Visit getNext() {
         return next;
     }
 
-    // setter method to link the next node
-    public void setNext(Visit next) {
-        this.next = next;
-    }
-
-    // getter method for a patient's appointment data
+    /**
+     * This method acts as a getter for the appointment that is being added
+     * The appointment's date information will be added to the linked list
+     * @return the appointment date that is being added into visit tracking
+     */
     public Appointment getAppointment () {
         return appointment;
     }
 
+    /**
+     * This method acts as a setter to link the current node to the next node
+     * The next node's address gets referenced in the current node
+     * @param next used as the address of the next node containing next visit
+     */
+    public void setNext(Visit next) {
+        this.next = next;
+    }
 }
 
